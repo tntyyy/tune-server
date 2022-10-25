99dddd5c5d156fba414507b904ad3689e18a080c
tnty <tnty.working@gmail.com>
2022-10-25 13:48:32 +0300

    feat: config & connect to db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./database");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server started. Port: ${PORT}`);
-});
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => {
+            console.log(`Server started. Port: ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
